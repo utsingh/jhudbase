7b1620ebd215ad1728bbd49610f1bccdee16f490
kevin-gorman <48705862+kevin-gorman@users.noreply.github.com>
2021-05-09 13:36:31 -0400

    Update conf.php

--- conf.php
+++ conf.php
@@ -4,6 +4,6 @@
 	//make updates here as noted below, and save this file as conf.php
 	$dbhost = "dbase.cs.jhu.edu"; 
 	$dbuser = "21sp_kgorman4";    
-	$dbpass = "XF15z1YTIo";            
+	$dbpass = "**********";            
 	$dbname = "21sp_kgorman4_db"; 
 ?>
