9a43ce6664221ec8d46091590415b252f35be2cb
Utkarsh Singh <usingh5@jhu.edu>
2021-05-09 23:11:12 +0530

    deleted conf.php

--- conf.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-	//these are credentials which will be used by open.php to create connection to dbase
-	//make updates here as noted below, and save this file as conf.php
-	$dbhost = "dbase.cs.jhu.edu"; 
-	$dbuser = "21sp_kgorman4";    
-	$dbpass = "**********";            
-	$dbname = "21sp_kgorman4_db"; 
-?>
