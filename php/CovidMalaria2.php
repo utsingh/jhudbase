141495bdd55737ed37f55b9086a408ea2adc56d8
Utkarsh Singh <usingh5@jhu.edu>
2021-05-12 04:40:24 +0530

    table removed

--- php/CovidMalaria2.php
+++ php/CovidMalaria2.php
@@ -52,7 +52,7 @@ if (empty($item)) {
             echo "No bids found for the specified item";
 
          } else {
-	 
+	 /*
             //Create table to display results
             echo "<table border=\"1px solid black\">";
             echo "<tr><th> country </th> <th> malaria_incidence </th> <th> confirmed </th><th> recovered </th><th> deaths </th></tr>";
@@ -66,13 +66,17 @@ if (empty($item)) {
                echo "<td>".$row[3]."</td>";
                echo "<td>".$row[4]."</td>";
                echo "</tr>";
-               array_push($dataPoints, array( "label"=> $row[0], "y"=> $row[1]));
+               
             } 
 
-
-         
 	 
             echo "</table>";
+
+         */
+            while ($row = $result->fetch_row()) {
+               array_push($dataPoints, array( "label"=> $row[0], "y"=> $row[1]));
+            }
+         
 
          }	 
 
